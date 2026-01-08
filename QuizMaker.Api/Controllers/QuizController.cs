71aed6a5b325c0f70c2e3b3298cbfe3352c11aa7
Duje Miljević <duje.miljevic@abysalto.eu>
2026-01-08 19:10:09 +0100

    added QuizController

---
+++ QuizMaker.Api/Controllers/QuizController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QuizMaker.Application.Dto.Requests;
+using QuizMaker.Application.Services;
+
+namespace QuizMaker.Api.Controllers
+{
+    [Route("api/v1/quizzes")]
+    [ApiController]
+    public class QuizController : ControllerBase
+    {
+        private readonly IQuizService _quizService;
+
+        public QuizController(IQuizService quizService)
+        {
+            _quizService = quizService;
+        }
+
+        [HttpPost]
+        [Route("create")]
+        public async Task<IActionResult> CreateQuiz(CreateQuizRequest request, CancellationToken cancellationToken)
+        {
+            await _quizService.CreateQuiz(request, cancellationToken);
+            return NoContent();
+        }
+    }
+}
