3c35a2311156e33deebdd6aac1bcf55b93c155a0
Duje Miljević <duje.miljevic@abysalto.eu>
2026-01-08 19:05:19 +0100

    added interface and implementation service

---
+++ QuizMaker.Application/Services/IQuizService.cs
@@ -0,0 +1,8 @@
+﻿using QuizMaker.Application.Dto.Requests;
+
+namespace QuizMaker.Application.Services;
+
+public interface IQuizService
+{
+    Task CreateQuiz(CreateQuizRequest request, CancellationToken cancellationToken);
+}
---
+++ QuizMaker.Infrastructure/Services/QuizService.cs
@@ -0,0 +1,12 @@
+﻿using QuizMaker.Application.Dto.Requests;
+using QuizMaker.Application.Services;
+
+namespace QuizMaker.Infrastructure.Services;
+
+public class QuizService : IQuizService
+{
+    public async Task CreateQuiz(CreateQuizRequest request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
