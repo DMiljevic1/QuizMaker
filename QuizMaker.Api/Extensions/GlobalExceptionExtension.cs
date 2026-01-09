6cd224cab095e6696ca17dde8c38b2ae7aaee023
Duje Miljević <duje.miljevic@abysalto.eu>
2026-01-09 17:51:44 +0100

    added serilog and global exception

---
+++ QuizMaker.Api/Extensions/GlobalExceptionExtension.cs
@@ -0,0 +1,13 @@
+﻿using QuizMaker.Api.Handlers;
+
+namespace QuizMaker.Api.Extensions
+{
+    public static class GlobalExceptionExtension
+    {
+        public static void AddGlobalException(this IServiceCollection services)
+        {
+            services.AddExceptionHandler<GlobalExceptionHandler>();
+            services.AddProblemDetails();
+        }
+    }
+}
---
+++ QuizMaker.Api/Extensions/SerilogExtension.cs
@@ -0,0 +1,11 @@
+﻿using Serilog;
+
+namespace QuizMaker.Api.Extensions;
+
+public static class SerilogExtension
+{
+    public static void AddSerilogLogging(this IServiceCollection services, WebApplicationBuilder builder)
+    {
+        builder.Host.UseSerilog((context, services, loggerConfig) => loggerConfig.ReadFrom.Configuration(context.Configuration));
+    }
+}
