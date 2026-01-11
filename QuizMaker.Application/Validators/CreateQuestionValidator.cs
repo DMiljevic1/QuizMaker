303acd888fb61ba534d2e76ec6e7c1e84dd50426
unknown <duje.miljevic@gmail.com>
2026-01-11 11:42:51 +0100

    updated CreateQuestionValidator

--- QuizMaker.Application/Validators/CreateQuestionValidator.cs
+++ QuizMaker.Application/Validators/CreateQuestionValidator.cs
@@ -9,10 +9,14 @@ public class CreateQuestionValidator : AbstractValidator<CreateQuestionRequest>
     {
         RuleFor(x => x.QuestionText)
             .NotEmpty()
-            .WithMessage("Question text is required.");
+            .WithMessage("Question text is required.")
+            .MaximumLength(1000)
+            .WithMessage("Question text must not exceed 1000 characters.");
 
         RuleFor(x => x.AnswerText)
             .NotEmpty()
-            .WithMessage("Answer text is required.");
+            .WithMessage("Answer text is required.")
+            .MaximumLength(500)
+            .WithMessage("Answer text must not exceed 500 characters.");
     }
 }
